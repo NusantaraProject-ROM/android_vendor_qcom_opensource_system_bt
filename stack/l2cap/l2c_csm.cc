a11a0b51a8bb5a44d7aaba318dc27e7fcffdad94
Bandari Ramesh <c_ramban@qti.qualcomm.com>
2017-07-12 13:51:01 +0530

    GAP: Don't initiate ACL connection in collision scenario.
    If stack informs the ACL collision to upper
    layers as failure, profiles may reinitiate connection
    immediately which may leads to collision again.
    
    Hence wait for incoming connection when ACL collision
    triggered by SOC
    
    Change-Id: I46ec32601c86b2f44e5ad0eaba44af6959f2873e
    CRs-Fixed: 2053443

--- stack/l2cap/l2c_csm.cc
+++ stack/l2cap/l2c_csm.cc
@@ -180,8 +180,8 @@ static void l2c_csm_closed(tL2C_CCB* p_ccb, uint16_t event, void* p_data) {
 
     case L2CEVT_LP_CONNECT_CFM_NEG: /* Link failed          */
       /* Disconnect unless ACL collision and upper layer wants to handle it */
-      if (p_ci->status != HCI_ERR_CONNECTION_EXISTS ||
-          !btm_acl_notif_conn_collision(p_ccb->p_lcb->remote_bd_addr)) {
+      if (((p_ci->status != HCI_ERR_CONNECTION_EXISTS) && (p_ci->status != HCI_ERR_CONTROLLER_BUSY))
+          || !btm_acl_notif_conn_collision(p_ccb->p_lcb->remote_bd_addr)) {
         L2CAP_TRACE_API(
             "L2CAP - Calling ConnectCfm_Cb(), CID: 0x%04x  Status: %d",
             p_ccb->local_cid, p_ci->status);
