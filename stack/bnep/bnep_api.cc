7a46edd3280dc876b12edd9bcc6a91fd0ebb978b
Hansong Zhang <hsz@google.com>
2018-04-11 16:04:51 -0700

    DO NOT MERGE Add bounds check for BNEP_Write
    Bug: 74947856
    Test: manual
    Change-Id: If5db8c6b6e509a330ae74808fc3f0ffac137af14
    (cherry picked from commit ae9d06c1dc84db36c0c4a07fc56a1fbf008cd1ce)
    (cherry picked from commit 353cdbe3b64c9514bb49f73529cd98ea3a501df5)

--- stack/bnep/bnep_api.cc
+++ stack/bnep/bnep_api.cc
@@ -23,6 +23,7 @@
  ******************************************************************************/
 
 #include "bnep_api.h"
+#include <log/log.h>
 #include <string.h>
 #include "bnep_int.h"
 
@@ -378,6 +379,10 @@ tBNEP_RESULT BNEP_WriteBuf(uint16_t handle, const RawAddress& p_dest_addr,
         protocol = 0;
       else {
         new_len += 4;
+        if (new_len > org_len) {
+          android_errorWriteLog(0x534e4554, "74947856");
+          return BNEP_IGNORE_CMD;
+        }
         p_data[2] = 0;
         p_data[3] = 0;
       }
@@ -474,6 +479,10 @@ tBNEP_RESULT BNEP_Write(uint16_t handle, const RawAddress& p_dest_addr,
         protocol = 0;
       else {
         new_len += 4;
+        if (new_len > org_len) {
+          android_errorWriteLog(0x534e4554, "74947856");
+          return BNEP_IGNORE_CMD;
+        }
         p_data[2] = 0;
         p_data[3] = 0;
       }
