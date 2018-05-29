7a0993a5cf1b2bef2e73ec7d4912d897b70a21e9
Jakub Pawlowski <jpawlowski@google.com>
2018-05-29 16:17:32 -0700

    Decrease length after reading from array in process_service_attr_req
    Test: compilation
    Bug: 78136677
    Change-Id: I4807a350e2b4764a93f104ce88f23a957a7e85c0
    (cherry picked from commit 6cd2e8bf6e5707e8e77e7aca6519c58200ee58db)

--- stack/sdp/sdp_server.cc
+++ stack/sdp/sdp_server.cc
@@ -683,9 +683,11 @@ static void process_service_attr_req(tCONN_CB* p_ccb, uint16_t trans_num,
 
   /* Extract the record handle */
   BE_STREAM_TO_UINT32(rec_handle, p_req);
+  param_len -= sizeof(rec_handle);
 
   /* Get the max list length we can send. Cap it at MTU size minus overhead */
   BE_STREAM_TO_UINT16(max_list_len, p_req);
+  param_len -= sizeof(max_list_len);
 
     if (max_list_len < SDP_MIN_ATTR_REQ_MAX_BYTE_COUNT)
     {
