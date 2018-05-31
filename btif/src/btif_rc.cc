ca7b6608cf720007f87ef6aa9848574da07fe0eb
Sumit Bajpai <sbajpai@codeaurora.org>
2018-05-31 18:19:21 +0530

    Add BR feature bit if SDP not done but remote in conf file.
    In cases where remote does incoming connection and DUT doesn't
    have peer supported features as SDP is not completed, enable
    BR feature bit in peer_features which is sent to application
    when avrcp connects. This will allow Avrcp to know that peer
    supports BR and can send proper track UID in interim response
    when remote registers for track_changed event.
    
    CRs-Fixed: 2256162
    Change-Id: I28e7084796fc464e3b00a082d1ba5ebcd5743c80

--- btif/src/btif_rc.cc
+++ btif/src/btif_rc.cc
@@ -1422,6 +1422,12 @@ void btif_rc_handler(tBTA_AV_EVT event, tBTA_AV* p_data) {
       }
     } break;
     case BTA_AV_RC_OPEN_EVT: {
+      int ver = AVRC_REV_INVALID;
+      ver = sdp_get_stored_avrc_tg_version(p_data->rc_open.peer_addr);
+      if (ver > AVRC_REV_1_3) {
+        BTIF_TRACE_IMP("%s: remote ver > 1.3, add BR feature bit", __func__);
+        p_data->rc_open.peer_features |= BTA_AV_FEAT_BROWSE;
+      }
       BTIF_TRACE_DEBUG("%s: Peer_features: %x", __func__,
                        p_data->rc_open.peer_features);
       handle_rc_connect(&(p_data->rc_open));
