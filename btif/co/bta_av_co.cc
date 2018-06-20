a58e236db931a04573df933947ef23a18da9ee16
Sagar Verma <sagarv@codeaurora.org>
2018-06-20 16:02:08 +0530

    A2DP: Don't recache peer config after reconfig complete
    For remote initiated incoming signalling don't cache the
    peer config once the reconfig is done as Peer, Audio and
    Encoder must be configured with same configuration while
    recaching peer config again after Reconfig is leading to
    Encoder caching the old Remote config again.
    
    Change-Id: I58a61716c4d420bef5c8946e8f29d08a00fdc73a

--- btif/co/bta_av_co.cc
+++ btif/co/bta_av_co.cc
@@ -128,6 +128,7 @@ typedef struct {
   bool cp_active;                        /* current CP configuration */
   bool acp;                              /* acceptor */
   bool reconfig_needed;                  /* reconfiguration is needed */
+  bool rcfg_done;                        /* reconfiguration complete */
   bool opened;                           /* opened */
   uint16_t mtu;                          /* maximum transmit unit size */
   uint16_t uuid_to_connect;              /* uuid of peer device */
@@ -372,6 +373,7 @@ void bta_av_co_audio_disc_res(tBTA_AV_HNDL hndl, uint8_t num_seps,
   p_peer->num_sinks = num_sink;
   p_peer->num_srcs = num_src;
   p_peer->num_seps = num_seps;
+  p_peer->rcfg_done = false;
   p_peer->num_rx_sinks = 0;
   p_peer->num_rx_srcs = 0;
   p_peer->num_sup_sinks = 0;
@@ -584,6 +586,7 @@ tA2DP_STATUS bta_av_co_audio_getconfig(tBTA_AV_HNDL hndl, uint8_t* p_codec_info,
       APPL_TRACE_DEBUG("%s: call BTA_AvReconfig(x%x)", __func__, hndl);
       BTA_AvReconfig(hndl, true, p_sink->sep_info_idx, p_peer->codec_config,
                      *p_num_protect, bta_av_co_cp_scmst);
+      p_peer->rcfg_done = true;
     }
   } else {
     *p_sep_info_idx = p_sink->sep_info_idx;
@@ -1117,10 +1120,12 @@ static tBTA_AV_CO_SINK* bta_av_co_audio_codec_selected(
   ** In case of acceptor or remote initiated connection need to update enocder with codec config
   ** details as sent by remote during set config
   */
-  if (p_peer->acp == true && isDevUiReq != true) {
-      /* check if sample rate or channel mode is zero */
-      if (( codec_config.ota_codec_peer_config_[9] != 0) && ( codec_config.ota_codec_peer_config_[10] != 0)) {
-        memcpy(codec_config.ota_codec_config_, codec_config.ota_codec_peer_config_, AVDT_CODEC_SIZE);
+  if (p_peer->acp == true && isDevUiReq != true  && p_peer->rcfg_done != true) {
+    /* check if sample rate or channel mode is non-zero (remote set-conf) and
+     * cache peer config for possibility of reconfiguation for better config */
+    if ((codec_config.ota_codec_peer_config_[9] != 0) && (codec_config.ota_codec_peer_config_[10] != 0)) {
+      memcpy(codec_config.ota_codec_config_, codec_config.ota_codec_peer_config_, AVDT_CODEC_SIZE);
+      APPL_TRACE_WARNING("%s: Cache remote config for encoder update on codec select ", __func__);
     }
   }
 
@@ -1344,6 +1349,7 @@ bool bta_av_co_set_codec_user_config(
     APPL_TRACE_DEBUG("%s: call BTA_AvReconfig(x%x)", __func__, p_peer->handle);
     BTA_AvReconfig(p_peer->handle, true, p_sink->sep_info_idx,
                    p_peer->codec_config, num_protect, bta_av_co_cp_scmst);
+    p_peer->rcfg_done = true;
   }
 
 done:
@@ -1511,6 +1517,7 @@ bool bta_av_co_set_codec_audio_config(
                        p_peer->handle);
       BTA_AvReconfig(p_peer->handle, true, p_sink->sep_info_idx,
                      p_peer->codec_config, num_protect, bta_av_co_cp_scmst);
+      p_peer->rcfg_done = true;
     }
   }
 
