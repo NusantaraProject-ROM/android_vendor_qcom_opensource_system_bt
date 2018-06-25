ee3a2228dcda2a2e125f4bb48df204b85715447f
xiaowang <xiaowang@codeaurora.org>
2018-06-25 14:52:37 +0800

    SMP: Initializing of auth complete status.
    Fix SMP pairing complete status uninitialized issue.
    CRs-fixed: 2259796
    
    Change-Id: I952a0bfb7ff4fc7702684fabe66f1f52f6091440

--- stack/smp/smp_act.cc
+++ stack/smp/smp_act.cc
@@ -1368,6 +1368,7 @@ void smp_br_send_pair_response(tSMP_CB* p_cb, tSMP_INT_DATA* p_data) {
 void smp_pairing_cmpl(tSMP_CB* p_cb, tSMP_INT_DATA* p_data) {
   if (p_cb->total_tx_unacked == 0) {
     /* process the pairing complete */
+    p_cb->status = *(uint8_t*)p_data;
     smp_proc_pairing_cmpl(p_cb);
   }
 }
