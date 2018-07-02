8b60fe865106a30dbc015fce47354cc363471c19
Venkata Jagadeesh Garaga <vgaraga@codeaurora.org>
2018-07-02 15:40:16 +0530

    GAP: Update sock handle properly in OOL
    In failure scenarios, sock handle not inilizied
    and l2cap sock free triggers, which wrongly fetching
    valid handle and closing.
    
    Hence Update sock handle properly in OOL
    
    Change-Id: I5d863c5f1b33f5f5dbd3d0af56fad72a822c4469
    CRs-Fixed: 2264364

--- btif/src/btif_sock_l2cap.cc
+++ btif/src/btif_sock_l2cap.cc
@@ -418,12 +418,11 @@ static void on_cl_l2cap_init(tBTA_JV_L2CAP_CL_INIT* p_init, uint32_t id) {
   sock = btsock_l2cap_find_by_id_l(id);
   if (!sock) return;
 
+  sock->handle = p_init->handle;
   if (p_init->status != BTA_JV_SUCCESS) {
     btsock_l2cap_free_l(sock);
     return;
   }
-
-  sock->handle = p_init->handle;
 }
 
 /**
