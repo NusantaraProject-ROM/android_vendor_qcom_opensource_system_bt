1138b094ae424db2488c9189749c15195528bb34
Satheesh Kumar Pallemoni <spallemo@codeaurora.org>
2019-04-04 14:33:22 +0530

    Do discover while incoming connection if remote does set_config on SBC
    While incoimg connection, if remote does set_config on SBC(may be remotes
    high priority codec instead other codecs), then we are not sending
    AVDTP_DISCOVER to know, what all additional codecs it supports, due to which
    DUT is HD codec option has been greyed out, although if remote supports the same.
    
    - To enable it, DUT needs to send discover_req like doing for all other codecs.
    
    - Removed dead code
    
    CRs-Fixed: 2419066
    Change-Id: I6c969911fff9ff335d3b9dd21c60d1f9dcc61ebe

--- bta/av/bta_av_aact.cc
+++ bta/av/bta_av_aact.cc
@@ -1532,15 +1532,13 @@ void bta_av_security_rsp(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
 void bta_av_setconfig_rsp(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
   uint8_t num = p_data->ci_setconfig.num_seid + 1;
   uint8_t avdt_handle = p_data->ci_setconfig.avdt_handle;
-  uint8_t* p_seid = p_data->ci_setconfig.p_seid;
-  int i;
   uint8_t local_sep;
 
   /* we like this codec_type. find the sep_idx */
   local_sep = bta_av_get_scb_sep_type(p_scb, avdt_handle);
   bta_av_adjust_seps_idx(p_scb, avdt_handle);
-  APPL_TRACE_DEBUG("%s: sep_idx: %d cur_psc_mask:0x%x", __func__,
-                   p_scb->sep_idx, p_scb->cur_psc_mask);
+  APPL_TRACE_DEBUG("%s: sep_idx: %d cur_psc_mask:0x%x, num: %d", __func__,
+                   p_scb->sep_idx, p_scb->cur_psc_mask, num);
 
   if ((AVDT_TSEP_SNK == local_sep) &&
       (p_data->ci_setconfig.err_code == AVDT_SUCCESS) &&
@@ -1572,37 +1570,11 @@ void bta_av_setconfig_rsp(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
     else
       p_scb->avdt_version = AVDT_VERSION;
 
-    if (A2DP_GetCodecType(p_scb->cfg.codec_info) == A2DP_MEDIA_CT_SBC || num > 1) {
-        /* For any codec used by the SNK as INT, discover req is not sent in bta_av_config_ind.
-         * This is done since we saw an IOT issue with APTX codec. Thus, we now take same
-         * path for all codecs as for SBC. call disc_res now */
-        /* this is called in A2DP SRC path only, In case of SINK we don't need it  */
-        if (local_sep == AVDT_TSEP_SRC)
-          p_scb->p_cos->disc_res(p_scb->hndl, num, num, 0, p_scb->peer_addr,
-                                 UUID_SERVCLASS_AUDIO_SOURCE);
-
-        for (i = 1; i < num; i++) {
-          APPL_TRACE_DEBUG("%s: sep_info[%d] SEID: %d", __func__, i, p_seid[i - 1]);
-          /* initialize the sep_info[] to get capabilities */
-          p_scb->sep_info[i].in_use = false;
-          p_scb->sep_info[i].tsep = AVDT_TSEP_SNK;
-          p_scb->sep_info[i].media_type = p_scb->media_type;
-          p_scb->sep_info[i].seid = p_seid[i - 1];
-        }
-
-        /* only in case of local sep as SRC we need to look for other SEPs, In case
-         * of SINK we don't */
-        if (local_sep == AVDT_TSEP_SRC) {
-          /* Make sure UUID has been initialized... */
-          if (p_scb->uuid_int == 0) p_scb->uuid_int = UUID_SERVCLASS_AUDIO_SOURCE;
-            bta_av_next_getcap(p_scb, p_data);
-        }
-    } else {
-        if (local_sep == AVDT_TSEP_SRC)
-          if (p_scb->uuid_int == 0) p_scb->uuid_int = UUID_SERVCLASS_AUDIO_SOURCE/*p_scb->open_api.uuid*/;
-            /* we do not know the peer device and it is using non-SBC codec
-             * we need to know all the SEPs on SNK */
-          bta_av_discover_req(p_scb, NULL);
+    if (local_sep == AVDT_TSEP_SRC) {
+      if (p_scb->uuid_int == 0) p_scb->uuid_int = UUID_SERVCLASS_AUDIO_SOURCE;
+        /* we do not know the peer device and it is using non-SBC codec
+         * we need to know all the SEPs on SNK */
+      bta_av_discover_req(p_scb, NULL);
     }
   }
 }
