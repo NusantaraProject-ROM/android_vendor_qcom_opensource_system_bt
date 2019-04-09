84a5ea057630ce597d8183ac15d391b0a11fd02d
Venkata Jagadeesh Garaga <vgaraga@codeaurora.org>
2019-04-09 10:53:50 +0530

    GAP: Validate the time formatting function output
    strftime time formatting function may give
    corrupted results if return value is zero.
    
    Hence always validate the return value of the strftime
    and then use formatted time string.
    
    Change-Id: I2f93d92c6c1a578cf721e432bf5979f2e84fb11a
    CRs-Fixed: 2422898

--- btif/src/btif_config.cc
+++ btif/src/btif_config.cc
@@ -215,10 +215,11 @@ static future_t* init(void) {
     time_t current_time = time(NULL);
     struct tm* time_created = localtime(&current_time);
     if (time_created) {
-      strftime(btif_config_time_created, TIME_STRING_LENGTH, TIME_STRING_FORMAT,
-               time_created);
-      config_set_string(config, INFO_SECTION, FILE_TIMESTAMP,
-                        btif_config_time_created);
+      if (strftime(btif_config_time_created, TIME_STRING_LENGTH,
+                   TIME_STRING_FORMAT, time_created)) {
+        config_set_string(config, INFO_SECTION, FILE_TIMESTAMP,
+                          btif_config_time_created);
+      }
     }
   }
 
