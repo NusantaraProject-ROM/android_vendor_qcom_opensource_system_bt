5cc6d1a81808568df1544ec7e62b6a0809d7381f
weichinweng <weichinweng@google.com>
2020-04-21 09:42:27 +0800

    NIAP: implement bluetooth keystore interface.(2/2)
    Bug: 148758680
    Test: m
    
    Change-Id: I89f782039123d1deac173d13670de4d9dae5252a
    Merged-In: I89f782039123d1deac173d13670de4d9dae5252a
    (cherry picked from commit bfbdd5740ac65e90f386bb0b18079fcab4463c2d)

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -64,6 +64,7 @@
 #include "btif_config.h"
 #include "device/include/controller.h"
 #include "btif_debug.h"
+#include "btif_keystore.h"
 #include "btif_storage.h"
 #include "device/include/device_iot_config.h"
 #include "btsnoop.h"
@@ -426,6 +427,8 @@ static const void* get_profile_interface(const char* profile_id) {
   if (is_profile(profile_id, BT_PROFILE_HEARING_AID_ID))
     return btif_hearing_aid_get_interface();
 
+  if (is_profile(profile_id, BT_KEYSTORE_ID))
+    return bluetooth::bluetooth_keystore::getBluetoothKeystoreInterface();
   return NULL;
 }
 
