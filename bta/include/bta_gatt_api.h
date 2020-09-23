a966fd1210772dc90a368f61a1754c45010ff08e
HsingYuan Lo <hylo@google.com>
2020-09-23 17:26:01 +0800

    Expose service changed event to application (1/3)
    1. Add onServiceChanged() into IBluetoothGattCallback (AIDL)
    2. Define tBTA_GATTC_SERVICE_CHANGED data type
    3. Modify the event caller in bta_gatt_act.cc by using new data type
    4. Modify the related usages for BTA_GATTC_SRVC_CHG_EVT
    5. Handle BTA_GATTC_SRVC_CHG_EVT in btif_gatt_client.cc
    6. Add service_change_cb into btgatt_client_callbacks_t
    7. Modify the related usages for btgatt_client_callbacks_t
    
    Bug: 154056389
    Tag: #feature
    Test: test service changed scenario
    Change-Id: Ib5f5dac52973e39b22c9e66ee08030d8db768624
    (cherry picked from commit 48309774a8fbab9279a78bcd39a0e6fede6e1525)

--- bta/include/bta_gatt_api.h
+++ bta/include/bta_gatt_api.h
@@ -201,6 +201,11 @@ typedef struct {
   tGATT_STATUS status;
 } tBTA_GATTC_CONN_UPDATE;
 
+typedef struct {
+  RawAddress remote_bda;
+  uint16_t conn_id;
+} tBTA_GATTC_SERVICE_CHANGED;
+
 typedef union {
   tGATT_STATUS status;
 
@@ -219,6 +224,7 @@ typedef union {
   tBTA_GATTC_CONGEST congest;
   tBTA_GATTC_PHY_UPDATE phy_update;
   tBTA_GATTC_CONN_UPDATE conn_update;
+  tBTA_GATTC_SERVICE_CHANGED service_changed;
 } tBTA_GATTC;
 
 /* GATTC enable callback function */
