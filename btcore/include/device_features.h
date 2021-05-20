4375194936a7e5f95ac6abf342782b25bd76badc
Venkata Jagadeesh Garaga <vgaraga@codeaurora.org>
2021-05-20 18:39:05 +0530

    Add support for Adv Audio host addon features
    - Provided feature mask to parse features from
      host add on features array list
    
    Change-Id: I198e015f30b8824c74fab9c011916da577908d04
    CRs-Fixed: 2957220

--- btcore/include/device_features.h
+++ btcore/include/device_features.h
@@ -23,6 +23,13 @@
 #define SOC_ADD_ON_FEATURES_MAX_SIZE 245
 #define HOST_ADD_ON_FEATURES_MAX_SIZE 255
 
+#define HOST_ADD_ON_ADV_AUDIO_UNICAST_FEAT_MASK(x) ((x)[0] & 0x01)
+#define HOST_ADD_ON_ADV_AUDIO_BCA_FEAT_MASK(x)     ((x)[0] & 0x02)
+#define HOST_ADD_ON_ADV_AUDIO_BCS_FEAT_MASK(x)     ((x)[0] & 0x04)
+#define HOST_ADD_ON_ADV_AUDIO_STEREO_RECORDING(x)  ((x)[0] & 0x08)
+#define HOST_ADD_ON_ADV_AUDIO_LC3Q_FEAT_MASK(x)    ((x)[0] & 0x10)
+#define HOST_ADD_ON_QHS_FEAT_MASK(x)               ((x)[0] & 0x20)
+
 // Represents a page of device feature enabled/disabled bits returned
 // by the local controller. See the bluetooth spec for bit indexes.
 typedef struct { uint8_t as_array[8]; } bt_device_features_t;
