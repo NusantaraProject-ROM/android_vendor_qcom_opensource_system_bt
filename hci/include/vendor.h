9cc396c13ab309b31c7278a2feb5dd8e1aa8af61
priyankar <prigup@codeaurora.org>
2021-06-21 16:11:23 +0530

    Add more crash reason.
    This patch contains changes for adding
    more crash reason.
    
    Change-Id: I7f029badd7d483fab76dcd50e59e36d3fa315164

--- hci/include/vendor.h
+++ hci/include/vendor.h
@@ -164,6 +164,7 @@ typedef enum {
   BT_HOST_REASON_UART_IOCTL_STUCK                =  0x35,
   BT_HOST_REASON_POWER_IOCTL_STUCK               =  0x36,
   BT_HOST_REASON_PATCH_CONFIG_CMD_STUCK          =  0x37,
+  BT_HOST_REASON_GET_APP_VER_CMD_STUCK           =  0x38,
 
   // UART driver crash reasons
   UART_REASON_DEFAULT                            =  0x51,
@@ -281,6 +282,7 @@ static secondary_reason secondary_crash_reason [] = {
 { BT_HOST_REASON_NVM_PATCH_READ_STUCK            ,  "NVM file open/read stuck"},
 { BT_HOST_REASON_UART_IOCTL_STUCK                ,  "UART IOCTL stuck"},
 { BT_HOST_REASON_PATCH_CONFIG_CMD_STUCK          ,  "Patch config cmd stuck"},
+{ BT_HOST_REASON_GET_APP_VER_CMD_STUCK           ,  "Get APP version cmd stuck"},
 { UART_REASON_DEFAULT                            ,  "UART reason Default"},
 { UART_REASON_INVALID_FW_LOADED                  ,  "UART invalid FW loaded"},
 { UART_REASON_CLK_GET_FAIL                       ,  "UART CLK get failed"},
