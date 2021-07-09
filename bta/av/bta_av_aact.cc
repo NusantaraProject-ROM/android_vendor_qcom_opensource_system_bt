74aacbf2b014de7012023522cbba9f59777c3d25
Naval saini <saini@codeaurora.org>
2021-07-09 13:34:49 +0530

    A2DP: Reset packet type when reconfig from aptX to non-aptX codec type
    During reconfig from aptX to non-aptX codec reset packet type suppot.
    
    CRs-Fixed: 3006101
    Change-Id: Ib892f7d6695ae159d07206539a2b4ddb978e158a

--- bta/av/bta_av_aact.cc
+++ bta/av/bta_av_aact.cc
@@ -2552,7 +2552,6 @@ void bta_av_str_stopped(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
   bool sus_evt = true;
   BT_HDR* p_buf;
   uint8_t policy = HCI_ENABLE_SNIFF_MODE;
-  char *codec_name = (char *)A2DP_CodecName(p_scb->cfg.codec_info);
 
   APPL_TRACE_ERROR("%s: audio_open_cnt=%d, p_data %p", __func__,
                    bta_av_cb.audio_open_cnt, p_data);
@@ -2640,10 +2639,6 @@ void bta_av_str_stopped(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
       bta_av_data.suspend = suspend_rsp;
       (*bta_av_cb.p_cback)(BTA_AV_STOP_EVT, &bta_av_data);
     }
-  }
-  if(strstr(codec_name, "aptX") != NULL) {
-    APPL_TRACE_WARNING("%s: Removing packet type limit for aptX codec stream stop", __func__);
-    AVDT_UpdateLinkPktType(p_scb->avdt_handle, btm_cb.btm_acl_pkt_types_supported);
   }
 }
 
@@ -3063,6 +3058,9 @@ void bta_av_start_ok(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
     APPL_TRACE_WARNING("%s: Changing packet type for aptX family codec stream", __func__);
     AVDT_UpdateLinkPktType(p_scb->avdt_handle, (btm_cb.btm_acl_pkt_types_supported |
                 HCI_PKT_TYPES_MASK_NO_3_DH1 | HCI_PKT_TYPES_MASK_NO_3_DH3 | HCI_PKT_TYPES_MASK_NO_3_DH5));
+  } else {
+    APPL_TRACE_WARNING("%s: Resetting/Removing packet type limit for non-aptX codec stream", __func__);
+    AVDT_UpdateLinkPktType(p_scb->avdt_handle, btm_cb.btm_acl_pkt_types_supported);
   }
 
   if (!bta_av_link_role_ok(p_scb, A2DP_SET_ONE_BIT))
